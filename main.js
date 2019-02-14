7f0584e715df1db910855051649543298df3a0e6
cryptolog <irfan.simsek.gop@gmail.com>
2019-02-14 23:25:09 +0100

    with_js_1412

--- main.js
+++ main.js
@@ -1,51 +1,117 @@
-/*-----------------------Slides-----------------------*/
-var slideIndex = 0;
-showSlides();
+document.addEventListener('DOMContentLoaded', function(){
+  /*-----------------------Slides-----------------------*/
+  var slideIndex = 0;
+  showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
 
-/*-----------------------Togl-----------------------*/
-let toglStatu = false;
+  /*-----------------------Togl-----------------------*/
+  let toglStatu = false;
 
-let myTogl = function(){
-  let gethomnav = document.querySelector(".hom-navim");
-  let gethomnavfirst = document.querySelector(".hom-nav-first");
-  let gethomnavlast = document.querySelector(".hom-nav-last");
-  // for ikon
-  let getLine1 = document.querySelector(".lineOne");
-  let getLine2 = document.querySelector(".lineTwo");
-  let getLine3 = document.querySelector(".lineThree");
-
-  if(toglStatu === false){
-    gethomnavfirst.style.visibility= "visible";
-    gethomnavlast.style.visibility= "visible";
-    gethomnav.style.height= "auto";
-
-    toglStatu = true;
+  let myTogl = function(){
+    let gethomnav = document.querySelector(".hom-navim");
+    let gethomnavfirst = document.querySelector(".hom-nav-first");
+    let gethomnavlast = document.querySelector(".hom-nav-last");
     // for ikon
-    getLine1.style.transform= "rotate(-45deg) translate(-6px, 6px)";
-    getLine2.style.opacity= "0";
-    getLine3.style.transform= "rotate(45deg) translate(-4px, -4px)";
-  }
-  else if(toglStatu === true){
-    gethomnavfirst.style.visibility= "hidden";
-    gethomnavlast.style.visibility= "hidden";    
-    gethomnav.style.height= "55px";
+    let getLine1 = document.querySelector(".lineOne");
+    let getLine2 = document.querySelector(".lineTwo");
+    let getLine3 = document.querySelector(".lineThree");
 
-    toglStatu = false;
-     // for ikon
-     getLine1.style.transform= "rotate(0) translate(0, 0)";
-     getLine2.style.opacity= "1";
-     getLine3.style.transform= "rotate(0) translate(0, 0)";
-   }
-}
+    if(toglStatu === false){
+      gethomnavfirst.style.visibility= "visible";
+      gethomnavlast.style.visibility= "visible";
+      gethomnav.style.height= "auto";
+
+      toglStatu = true;
+      // for ikon
+      getLine1.style.transform= "rotate(-45deg) translate(-6px, 6px)";
+      getLine2.style.opacity= "0";
+      getLine3.style.transform= "rotate(45deg) translate(-4px, -4px)";
+    }
+    else if(toglStatu === true){
+      gethomnavfirst.style.visibility= "hidden";
+      gethomnavlast.style.visibility= "hidden";    
+      gethomnav.style.height= "55px";
+
+      toglStatu = false;
+      // for ikon
+      getLine1.style.transform= "rotate(0) translate(0, 0)";
+      getLine2.style.opacity= "1";
+      getLine3.style.transform= "rotate(0) translate(0, 0)";
+    }
+  }
+
+  //-----------------------------------ADD Comments---------------
+  document.querySelector('#addCom').addEventListener("submit", function(e)  {
+    e.preventDefault();
+    
+    let spanDislike=document.createElement('span');
+    let buttonDislike=document.createElement('button');
+    let liDislike=document.createElement('li');
+    let spanLike=document.createElement('span');
+    let buttonLike=document.createElement('button'); //butonlara isim eklemelisin
+    let liLike=document.createElement('li');
+    let ulComments=document.createElement('ul');
+    let like=document.createTextNode('Like');
+    let dLike=document.createTextNode('Dislike');
+    let divLikeDisLike=document.createElement('div');
+    let spanCount=document.createElement('span');
+    let count=document.createTextNode('0');
+    spanCount.appendChild(count);  
+    let spanCountL=document.createElement('span');
+    let countL=document.createTextNode('0');
+    spanCountL.appendChild(countL);
+
+    let commentP=document.createElement('p');
+    let texP=document.querySelector('#takeCom').value;
+    let commentPDiv=document.createElement('div');
+
+    let commentBoxDiv=document.createElement('div');
+
+    //--addClasses--
+    commentBoxDiv.classList.add('commentsBox');
+    commentPDiv.classList.add('comments');
+    divLikeDisLike.classList.add('commentsIcons');
+    spanCount.classList.add('btnSpan');
+    spanCountL.classList.add('btnSpan');
+
+    buttonDislike.style.marginLeft='10px';
+
+    //--addElements--
+    spanLike.appendChild(like);
+    buttonLike.appendChild(spanLike);
+    liLike.appendChild(buttonLike);
+    liLike.appendChild(spanCountL);
+    spanDislike.appendChild(dLike);
+    buttonDislike.appendChild(spanDislike);
+    liDislike.appendChild(buttonDislike);
+    liDislike.appendChild(spanCount);
+
+    ulComments.appendChild(liLike);
+    ulComments.appendChild(liDislike);
+    divLikeDisLike.appendChild(ulComments);
+
+    commentP.textContent=texP;
+    commentPDiv.appendChild(commentP);
+
+    commentBoxDiv.appendChild(commentPDiv);
+    commentBoxDiv.appendChild(divLikeDisLike);
+    // if eklenebilir...
+
+    let sectionRightDiv=document.querySelector('#sr');
+    let insertPlace=document.querySelector('#formDiv');
+    sectionRightDiv.insertBefore(commentBoxDiv, insertPlace);
+  })
+
+ 
+})
